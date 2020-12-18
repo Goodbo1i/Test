b7bb4887db00e1dd4622ee2f6394ccdddf26de50
Arminas Vilunas <vilunas.arminas@gmail.com>
2020-12-18 18:40:16 +0200

    Baigtas

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,82 @@ namespace Test
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(88, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Skaiciuoti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 327);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 302);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Iveskite skaiciu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(416, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Truksta 10 Skaiciu!";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(419, 95);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(262, 281);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,58 @@ namespace Test
 {
     public partial class Form1 : Form
     {
+        double[] masyvas = new double[10];
+        int counter = 0;
+        
+
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            double a = Convert.ToDouble(textBox1.Text);
+            if (counter < 9)
+            {
+                int counterbackwards = 9 - counter;
+                label2.Text = "Truksta " + counterbackwards + " skaiciu! Papildykite masyva!";
+
+                masyvas[counter] = a;
+
+                for(int i = 0; i <= counter; i++)
+                {
+                    richTextBox1.AppendText(String.Join(" ",masyvas[i]));
+                    richTextBox1.AppendText(" ");
+
+                }
+                richTextBox1.AppendText(Environment.NewLine);
+                counter++;
+
+            }else if(counter == 9)
+            {
+                masyvas[counter] = a;
+                counter++;
+                label2.Text = "Masyve yra " + counter + " skaiciu! Masyvas uzpildytas";
+
+                //richTextBox1.Text = String.Join("Masyvo suma: ", masyvas.Sum()); //, " Masyvo suma: ", masyvas.Sum()
+
+                richTextBox1.AppendText("Masyvo suma: "); 
+                richTextBox1.AppendText(String.Join("", masyvas.Sum()));
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
